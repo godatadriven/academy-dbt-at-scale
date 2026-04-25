65d69785309d9597d170dcda7bf9414b9503f6ab
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2026-04-25 16:56:36 +0200

    remove season and ep columns

--- mediapulse/models/staging/podcasts/stg_podcasts__episodes.sql
+++ mediapulse/models/staging/podcasts/stg_podcasts__episodes.sql
@@ -12,9 +12,7 @@ renamed as (
         episode_name as episode_title,
         cast(published_at as timestamp) as published_at,
         duration_seconds,
-        cast(episode_number as varchar)
-            || '-' ||
-        cast(season as varchar) as season_episode
+        season_episode
 
     from source
 
