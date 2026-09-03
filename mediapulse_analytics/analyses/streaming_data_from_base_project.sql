02b064dd43e1d2aa27195ac3f9a98e999d774ab8
Sophie Beerepoot <sophia.beerepoot@persgroep.net>
2026-09-03 12:19:47 +0000

    fix test and query data from base project

---
+++ mediapulse_analytics/analyses/streaming_data_from_base_project.sql
@@ -0,0 +1,8 @@
+select *
+from {{ ref('mediapulse_base', 'stg_streaming__content_ctlg') }}
+
+select *
+from {{ ref('mediapulse_base', 'stg_streaming__subscriptions_lifecycle_rec') }}
+
+select *
+from {{ ref('mediapulse_base', 'stg_streaming__usr_watch_events_log') }}
