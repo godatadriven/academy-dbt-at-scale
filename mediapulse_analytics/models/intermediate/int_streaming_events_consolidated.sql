2b8585984ab83c72909e8d5106454989b680a5cb
Auke Helleman <auke.helleman@persgroep.net>
2026-09-03 13:03:05 +0000

    add is_legacy booleans

--- mediapulse_analytics/models/intermediate/int_streaming_events_consolidated.sql
+++ mediapulse_analytics/models/intermediate/int_streaming_events_consolidated.sql
@@ -1,5 +1,6 @@
-(select * from {{ ref('mediapulse_base', 'stg_streaming__usr_watch_events_log') }})
+(select *, false as is_legacy
+from {{ ref('mediapulse_base', 'stg_streaming__usr_watch_events_log') }})
 union all
 (select
-    ping_id, legacy_subscriber_id, legacy_content_id, watched_at, playback_position_seconds, device_code, null as batched_at
+    ping_id, legacy_subscriber_id, legacy_content_id, watched_at, playback_position_seconds, device_code, null as batched_at, true as is_legacy
 from {{ ref("stg_streamview_legacy__watch_pings") }})
--- mediapulse_analytics/models/intermediate/int_streaming_subscriptions_consolidated.sql
+++ mediapulse_analytics/models/intermediate/int_streaming_subscriptions_consolidated.sql
@@ -1,3 +1,3 @@
-select * from {{ ref('mediapulse_base', 'stg_streaming__content_ctlg') }}
+select *, false as is_legacy from {{ ref('mediapulse_base', 'stg_streaming__content_ctlg') }}
 union all
-select * from {{ ref("stg_streamview_legacy__content") }}
+select *, true as is_legacy from {{ ref("stg_streamview_legacy__content") }}
